9ff5757ea6fe503096ae690a0ef59c815d123115
Karina Pivtorak <pivtorak.k@handy.ai>
2021-02-21 17:07:52 +0200

    create store class

--- store.py
+++ store.py
@@ -1,30 +1,33 @@
-class Store :    
-        
-    discount_rate = 0.2
-    
-    def __init__(self, company_name, phones_amount):
-        self.store_name = store_name   
-        self.phones_amount = phones_amount
-             
+from datetime import date
+
+
+class Store:
+
+    phones_amount = 5
+
+    def __init__(self, store_name):
+        self.store_name = store_name
+
     def __str__(self):
         """Return a descriptive string for this instance, invoked by print() and str()"""
-        return f'This is a store {self.store_name}. Now available {self.phones_amount} phones' 
-    
-    # def discount_price(self, discount):
-    #     return self.price*(1-discount)
-    
+        return f'This is a store {self.store_name}. Now available {self.phones_amount} phones'
+
     @classmethod
     def buy_phone(cls, rate):
-        cls.discount_rate = rate
+        cls.phones_amount -= 1
 
     @staticmethod
-    def is_shop_closed(day):
-        print(day.weekday())
-        if (day.weekday() == 7 | day.weekday() == 6):
-            return False
-        return True
-        
+    def is_shop_open():
+        week = date.today().weekday()
+        return not(week == 5 or week == 6)
+
+
 if __name__ == '__main__':
-    
-    store1 = Store()
-    print()
+
+    store1 = Store("KariStore")
+    print(store1)
+
+    # @staticmethod does not have access to an instance of the class, so
+    # it can be called without instantiating the class
+    print(store1.is_shop_open()) 
+    print(Store.is_shop_open())
